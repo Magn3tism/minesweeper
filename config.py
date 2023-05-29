09e3e46f7fa3ad45aa9170b962dc117710494d4c
magn3tism <chapagainsakshyam@gmail.com>
2023-05-29 13:51:25 +0545

    Mines placed

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-buttons = [[{"button": None, "content": None} for i in range(6)] for i in range(13)]
+buttons = [[{"button": None, "content": 0} for i in range(6)] for i in range(13)]
--- minesweeper.py
+++ minesweeper.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import ttkbootstrap as ttk
 import random
 
-import config
+from config import buttons
 
 window = ttk.Window(themename="flatly")
 window.title("Minesweeper")
@@ -10,15 +10,30 @@ window.title("Minesweeper")
 container = ttk.Frame(master=window)
 container.pack(padx=5, pady=5)
 
+def clicked_index(i, j):
+    print(buttons[i][j]["content"])
+
 for i in range(13):
     row_frame = ttk.Frame(master=container)
     row_frame.grid()
 
     for j in range(6):
-        config.buttons[i][j]['button'] = ttk.Button(
-            master=row_frame, bootstyle="primary-outline")
-        config.buttons[i][j]['button'].grid(ipadx=15, ipady=10, row=i, column=j)
+        buttons[i][j]["button"] = ttk.Button(
+            master=row_frame, bootstyle="primary-outline", 
+            command=lambda i=i, j=j: clicked_index(i, j))
+        
+        buttons[i][j]["button"].grid(ipadx=15, ipady=10, row=i, column=j)
 
+
+count = 0;
+
+while (count < 10):
+    a = random.randint(0, 12)
+    b = random.randint(0, 5)
+    
+    if (not buttons[a][b]["content"]):
+        buttons[a][b]["content"] = -1;
+        count += 1
 
 # Start main loop
 window.mainloop()
