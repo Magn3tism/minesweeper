bb4288ea4ce51600d95b2f4b4f39037a32ccfbab
magn3tism <chapagainsakshyam@gmail.com>
2023-05-29 12:47:25 +0545

    Change button and add global variable

---
+++ config.py
@@ -0,0 +1,1 @@
+buttons = [[{"button": None, "content": None} for i in range(6)] for i in range(13)]
--- minesweeper.py
+++ minesweeper.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 import ttkbootstrap as ttk
+import random
+
+import config
 
 window = ttk.Window(themename="flatly")
 window.title("Minesweeper")
-
-buttons = [[[] for i in range(6)] for i in range(13)]
 
 container = ttk.Frame(master=window)
 container.pack(padx=5, pady=5)
@@ -14,9 +15,10 @@ for i in range(13):
     row_frame.grid()
 
     for j in range(6):
-        buttons[i][j] = ttk.Button(
+        config.buttons[i][j]['button'] = ttk.Button(
             master=row_frame, bootstyle="primary-outline")
-        buttons[i][j].grid(ipadx=15, ipady=10, row=i, column=j)
+        config.buttons[i][j]['button'].grid(ipadx=15, ipady=10, row=i, column=j)
 
 
+# Start main loop
 window.mainloop()
