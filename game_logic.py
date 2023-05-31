9174f28d671d5a8d4e6ff5af57952b4747d26f50
magn3tism <chapagainsakshyam@gmail.com>
2023-05-31 19:18:12 +0545

    Auto invoke when there are no mines nearby

--- game_logic.py
+++ game_logic.py
@@ -4,6 +4,20 @@ from PIL import ImageTk, Image
 from config import buttons
 
 
+def get_surrounding_elements(matrix, row, col):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    surrounding_values = []
+
+    for i in range(max(0, row - 1), min(row + 2, rows)):
+        for j in range(max(0, col - 1), min(col + 2, cols)):
+            if i != row or j != col:
+                surrounding_values.append(matrix[i][j])
+
+    return surrounding_values
+
+
 def hit_mine():
     print("Game Over!")
     for row in buttons:
@@ -16,8 +30,13 @@ def hit_mine():
             btn["button"].configure(state="disabled")
 
 
-def click_number():
-    pass
+def click_number(i, j):
+    surrounding = get_surrounding_elements(buttons, i, j)
+
+    for btn in surrounding:
+        if (btn["content"] == 0):
+            btn["content"] = 100
+            btn["button"].invoke()
 
 
 def left_click(i, j):
@@ -25,9 +44,14 @@ def left_click(i, j):
     if (value == -2):
         return
 
+    if (value == 100 or value == 0):
+        buttons[i][j]["button"].configure(style="secondary.TButton")
+        click_number(i, j)
+        return
+
     if (value >= 0):
         image = ImageTk.PhotoImage(Image.open(f"./images/{value}.png"))
-        click_number()
+        click_number(i, j)
     else:
         image = ImageTk.PhotoImage(Image.open("./images/mine.png"))
         hit_mine()
