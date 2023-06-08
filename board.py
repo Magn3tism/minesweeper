eabe819cce7caa9f96b18206fb15bc5a9385ad9d
magn3tism <chapagainsakshyam@gmail.com>
2023-06-08 15:26:30 +0545

    Create a basic window

--- board.py
+++ board.py
@@ -1,6 +1,17 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtCore
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Minesweeper")
+        self.setMinimumSize(QSize(500, 300))
+
 
 app = QApplication([])
 
-window = QWidget()
+window = MainWindow()
 window.show()
