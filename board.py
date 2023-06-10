1cf6cc504a75e566385971a315da91f03fc34740
magn3tism <chapagainsakshyam@gmail.com>
2023-06-10 15:37:52 +0545

    Create grids

--- board.py
+++ board.py
@@ -1,17 +1,36 @@
-from PyQt6 import QtCore
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QGridLayout, QWidget
+from config import buttons
 
 
-class MainWindow(QMainWindow):
+class GameBoard(QMainWindow):
     def __init__(self):
         super().__init__()
+        global buttons
 
         self.setWindowTitle("Minesweeper")
-        self.setMinimumSize(QSize(500, 300))
+
+        self.layout = QGridLayout()
+        self.layout.setHorizontalSpacing(0)
+        self.layout.setVerticalSpacing(0)
+        self.add_buttons()
+
+        widget = QWidget()
+        widget.setLayout(self.layout)
+        self.setCentralWidget(widget)
+
+    def left_click(self, i, j):
+        pass
+
+    def add_buttons(self):
+        for i in range(13):
+            for j in range(6):
+                buttons[i][j]["content"] = QPushButton("")
+                buttons[i][j]["content"].clicked.connect(
+                    lambda event, i=i, j=j: self.left_click(i, j))
+                self.layout.addWidget(buttons[i][j]["content"], i, j)
 
 
 app = QApplication([])
 
-window = MainWindow()
+window = GameBoard()
 window.show()
